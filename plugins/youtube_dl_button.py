45ce366350a68d9261249864d199613e9a13800b
sukru262 <63332971+sukru262@users.noreply.github.com>
2020-04-08 21:13:27 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -322,9 +322,9 @@ async def youtube_dl_call_back(bot, update):
             media_album_p = []
             if images is not None:
                 i = 0
-                caption = "© @TGBotsZ"
+                caption = "😜 @Sk_Bots"
                 if is_w_f:
-                    caption = "/upgrade to Plan D to remove the watermark\n© @AnyDLBot"
+                    caption = "🥳"
                 for image in images:
                     if os.path.exists(image):
                         if i == 0:
